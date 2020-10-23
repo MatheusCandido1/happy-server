9eed770f905e2c3caccd748f3d4d5053dbc3dc62
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-23 13:32:09 -0300

    add-index-orphanages

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
+import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
-routes.post('/orphanages', async (request, response) => {
-   
-    
-});
+routes.post('/orphanages', OrphanagesController.create);
+
+routes.get('/orphanages', OrphanagesController.index);
 
 export default routes;
