97e1e7960f370a95447a24ed519a6996646521fc
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-23 14:58:07 -0300

    add-validations

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -5,6 +5,8 @@ import orphanageView from '../views/orphanages_view';
 import Orphanage from '../models/Orphanage';
 import orphanages_view from '../views/orphanages_view';
 
+import * as Yup from 'yup';
+
 export default {
     async show(request: Request, response: Response) {
         const { id } = request.params;
@@ -42,8 +44,8 @@ export default {
 
         const requestImages = request.files as Express.Multer.File[];
         const images = requestImages.map(image => {return {path: image.filename}})
-    
-        const orphanage = orphanagesRepository.create({
+
+        const data = {
             name,
             latitude,
             longitude,
@@ -52,7 +54,28 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        };
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                path: Yup.string().required()
+                })
+            )
         });
+
+        await schema.validate(data, {
+            abortEarly: false
+        });
+    
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
         
